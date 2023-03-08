fb0f560a22bd20e36d417aa037b72654513e85e8
totichi <jtmarquez@uc.cl>
2023-03-08 13:26:27 -0300

    fix: add secret to env

--- uc_grades_to_gpa/uc_grades_to_gpa/settings.py
+++ uc_grades_to_gpa/uc_grades_to_gpa/settings.py
@@ -11,16 +11,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dotenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Add .env variables anywhere before SECRET_KEY
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$)1)u%j(iwlx45a(&nxxgxv$0!+@42)s00!%=!4g^b84$agyc3'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
